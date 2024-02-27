1ce60f772ac5b0063b95b83cc6c8cf23be8f5aa7
ammonelzinga <ammonelzinga@gmail.com>
2024-02-26 20:32:58 -0700

    inital server testing

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,9 +1,18 @@
 import chess.*;
 import server.Server;
 
+
+//public static void main(String[] args)
 public class Main {
     public static void main(String[] args) {
+        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.println("♕ 240 Chess Server: " + piece);
+        new Main().run();
+    }
+
+    private void run(){
         //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        System.out.print("hello");
         Server server = new Server();
         int port = 8080;
         //System.out.println("♕ 240 Chess Server: " + piece);
